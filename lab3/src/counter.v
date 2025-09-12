db3bbe69beed3edc987cb8b3081a962f5446e27e
wangzhongyuan <wangzhongyuan1111@gmail.com>
2025-09-12 14:37:02 +0800

    clone labs

---
+++ lab3/src/counter.v
@@ -0,0 +1,22 @@
+module counter #(
+    parameter CYCLES_PER_SECOND = 125_000_000
+)(
+    input clk,
+    input [3:0] buttons,
+    output [3:0] leds
+);
+    reg [3:0] counter = 0;
+    assign leds = counter;
+
+    always @(posedge clk) begin
+        if (buttons[0])
+            counter <= counter + 4'd1;
+        else if (buttons[1])
+            counter <= counter - 4'd1;
+        else if (buttons[3])
+            counter <= 4'd0;
+        else
+            counter <= counter;
+    end
+endmodule
+
