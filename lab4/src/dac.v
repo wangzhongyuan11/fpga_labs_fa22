483cda837f605f2f45fb2599d0ffea9c42a1d4e1
wangzhongyuan <wangzhongyuan1111@gmail.com>
2025-09-16 12:57:39 +0800

    lab4

--- lab4/src/dac.v
+++ lab4/src/dac.v
@@ -3,11 +3,38 @@ module dac #(
     parameter CODE_WIDTH = $clog2(CYCLES_PER_WINDOW)
 )(
     input clk,
-    input rst,
     input [CODE_WIDTH-1:0] code,
+    input rst,
     output next_sample,
-    output reg pwm
+    output pwm
 );
-    assign pwm = 0;
-    assign next_sample = 0;
+    reg pwm_r = 0;
+    reg [CODE_WIDTH-1:0] code_r = 0;
+    reg [CODE_WIDTH-1:0] code_cnt = 0;
+
+    always @(posedge clk) begin
+        if(rst) begin
+            code_cnt <= 0;
+            pwm_r <= 0;
+        end 
+        else if(code_cnt == CYCLES_PER_WINDOW)
+            code_cnt <= 0;
+        else
+            code_cnt <= code_cnt + 1;
+    end
+
+    always @(*) begin
+        if(code_r == 0)
+            pwm_r = 0;
+        else
+            pwm_r = (code_cnt <= code_r) ? 1'b1 : 1'b0;
+    end
+
+    always @(posedge clk) begin
+        code_r <= code;
+    end
+
+    assign next_sample = (code_cnt == (CYCLES_PER_WINDOW-1));
+    assign pwm = pwm_r;
+
 endmodule
--- lab4/src/sq_wave_gen.v
+++ lab4/src/sq_wave_gen.v
@@ -8,6 +8,25 @@ module sq_wave_gen #(
     output [9:0] code,
     output [3:0] leds
 );
-    assign code = 0;
-    assign leds = 0;
+    localparam high_low_switch_COUNT = 139;
+    localparam HIGH_S = 1;
+    localparam HIGH_CODE = 562;
+    localparam LOW_S = 0;
+    localparam LOW_CODE = 462;
+    reg [$clog2(high_low_switch_COUNT)-1:0] dac_next_sample_cnt = 0;
+    reg code_state_r = LOW_S;
+
+    always @(posedge clk) begin
+        if (next_sample == 1'b1)
+            dac_next_sample_cnt <=  (dac_next_sample_cnt == high_low_switch_COUNT) ? 0:dac_next_sample_cnt + 1;
+        else
+            dac_next_sample_cnt <= dac_next_sample_cnt;
+        if (dac_next_sample_cnt == high_low_switch_COUNT)
+            code_state_r <= ~code_state_r;
+        else
+            code_state_r <= code_state_r;
+    end    
+
+    assign code = code_state_r == HIGH_S ? HIGH_CODE : LOW_CODE;
 endmodule
+
